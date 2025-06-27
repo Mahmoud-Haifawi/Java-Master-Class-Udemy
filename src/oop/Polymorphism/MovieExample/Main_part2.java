c579f63cb4a63456e6190c79a12f23130813fa68
Mahmoud Haifawi <mahmood.h.20@hotmail.com>
2025-06-28 00:04:32 +0300

    package oop.Polymorphism.Examples;

--- src/oop/Polymorphism/MovieExample/Main_part2.java
+++ src/oop/Polymorphism/MovieExample/Main_part2.java
@@ -14,11 +14,38 @@ public class Main_part2
         Comedy comedyMovie = (Comedy) comedy;
         comedyMovie.watchComedy();
 
+// Type inferred as Movie (return type of getMovie)
         var airplane = Movie.getMovie("C", "Airplane");
-        airplane.watchMovie();
+        airplane.watchMovie(); // Works
+       // airplane.watchComedy(); // COMPILATION ERROR - still inferred as Movie
 
+// Type inferred as Comedy (constructor type)
         var plane = new Comedy("Airplane");
-        plane.watchComedy();
+        plane.watchComedy(); // Works - compiler knows it's Comedy
+
+
+
+        /*
+
+            Compile-Time Type (Reference Type):
+
+            - Declared type of variable/parameter/return type
+            - Determines which methods can be called
+            - Known at compilation
+            - Left side of assignment operator
+
+            Runtime Type (Actual Object Type):
+
+            - Actual class of object in memory
+            - Determines which method implementation executes
+            - Only known during execution
+            - Right side of assignment operator
+        * */
+        Movie movie2 = Movie.getMovie("A", "Jaws"); // Movie = compile-time, Adventure = runtime
+        movie2.watchMovie(); // Calls Adventure.watchMovie() due to runtime type
+
     }
+
+
 
 }
