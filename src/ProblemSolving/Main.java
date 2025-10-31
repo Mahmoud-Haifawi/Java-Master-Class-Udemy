d3b47ad88de67eea16147d0408d569fd492ac3a7
Mahmoud-Haifawi <mahmoud.h.20@hotmail.com>
2025-10-31 11:25:07 +0300

    iteratir

--- src/ProblemSolving/Main.java
+++ src/ProblemSolving/Main.java
@@ -1,24 +1,87 @@
 package ProblemSolving;
-
-
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
-        int n1= scanner.nextInt();
-        int n2= scanner.nextInt();
-        int n3= scanner.nextInt();
-        int n4= scanner.nextInt();
-        System.out.println((n1*n2*n3*n4)%100);
+        int nnum = scanner.nextInt();
+        int orgginal=nnum;
+        int rev=0;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       while(nnum!=0)
+       {
+           rev=(rev*10)+nnum%10;
+           nnum=nnum/10;
 
-    }
-
+       }
+        System.out.println(rev);
+       if(rev==orgginal){
+           System.out.println("YES");
+       }
+       else
+           System.out.println("NO");
 
 }
+}
+/*
+   long pN=0;
+        long nN=0;
+        long evens=0;
+        long odds=0;
+
+        long n2=0;
+        for(long i=0;i<n1;i++)
+        {
+            n2= scanner.nextLong();
+            if(n2%2==0)
+                evens++;
+            else
+                odds++;
+            if(n2<0){
+                nN++;}
+            else {
+                if(n2!=0){
+                pN++;}
+            }
+
+
+        }
+
+        System.out.println("Even: " + evens);
+        System.out.println("Odd: " + odds);
+        System.out.println("Positive: " + pN);
+        System.out.println("Negative: " + nN);
+
+
+    }   long pN=0;
+        long nN=0;
+        long evens=0;
+        long odds=0;
+
+        long n2=0;
+        for(long i=0;i<n1;i++)
+        {
+            n2= scanner.nextLong();
+            if(n2%2==0)
+                evens++;
+            else
+                odds++;
+            if(n2<0){
+                nN++;}
+            else {
+                if(n2!=0){
+                pN++;}
+            }
+
+
+        }
+
+        System.out.println("Even: " + evens);
+        System.out.println("Odd: " + odds);
+        System.out.println("Positive: " + pN);
+        System.out.println("Negative: " + nN);
+
+
+    }
+* */
